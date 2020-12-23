892c976da19a7a493e526caff862a14dfbcef2a1
Blake Mercer <blake.mercer93@gmail.com>
2020-12-23 12:50:52 -0500

    picture in picture widget

---
+++ README.md
@@ -0,0 +1,7 @@
+picture-in-picture-widget
+PictureInPicture app built with JS and Media Devices WEB API.
+
+Propmts the user to select the media stream source on button click.
+Dynamically inserting the selected media stream into the DOM.
+Picture in Picture mode is enabled by clicking on the second button.
+Responsive on large and small screens.
---
+++ index.js
@@ -0,0 +1,54 @@
+const videoElement = document.getElementById('video');
+const startBtn = document.getElementById('start');
+const stopBtn = document.getElementById('stop');
+const selectBtn = document.getElementById('select');
+
+// Prompt user to select media stream, pass to video element, then play
+
+const selectMediaStream = async () => {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
+    videoElement.onloadedmetadata = () => {
+      videoElement.play();
+    };
+    selectBtn.hidden = true;
+    startBtn.hidden = false;
+  } catch (err) {
+    console.log('Whoops Error', err);
+  }
+};
+
+stopStreamedVideo = (videoElement) => {
+  const mediaStream = videoElement.srcObject;
+  const tracks = mediaStream.getTracks();
+
+  tracks.forEach((track) => {
+    track.stop();
+  });
+};
+
+// Start Picture-in-Picture
+const startPIP = async () => {
+  startBtn.hidden = true;
+  stopBtn.hidden = false;
+  await videoElement.requestPictureInPicture();
+};
+
+// Stop Picture-in-Picture
+const stopPIP = () => {
+  stopStreamedVideo(videoElement);
+  stopBtn.hidden = true;
+  selectBtn.hidden = false;
+  // Closes the Picture In Picture window
+  document.exitPictureInPicture();
+};
+
+// Select
+selectBtn.addEventListener('click', selectMediaStream);
+
+// Start
+startBtn.addEventListener('click', startPIP);
+
+// Stop
+stopBtn.addEventListener('click', stopPIP);
